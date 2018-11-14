c6007500e1359e09e3d4f140c46b6b8b4979eb64
Garret Harp <180988@s020rmc148-01.local>
2018-11-14 11:53:12 -0600

    Adding map feature

---
+++ GitTutorial/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitTutorial
+//
+//  Created by Harp, Garret C on 11/14/18.
+//  Copyright © 2018 Harp, Garret C. All rights reserved.
+//
+
+import Foundation
+
+class MapForItinerary: NSObject {
+    
+}
